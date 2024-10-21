0ae7fb20c119d164360a8d77ee1155f7eae159cf
eSalazar84 <salazaremiliano84@gmail.com>
2024-10-21 14:56:22 -0300

    se crean expedientes desde el cliente

---
+++ src/features/add_expedientes/NuevoExpediente.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormNewExp from '../../components/form_new_expediente/FormNewExp'
+
+function NuevoExpediente() {
+    return (
+        <>
+            <FormNewExp />
+        </>
+    )
+}
+
+export default NuevoExpediente
